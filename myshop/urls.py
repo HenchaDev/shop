73e99848e91dc0d6061c20ba09fc01371a450fe2
HenchaDev <henchaedits01@gmail.com>
2023-06-15 12:10:12 +0300

    Second commit

--- myshop/urls.py
+++ myshop/urls.py
@@ -1,1 +1,7 @@
 from django.urls import path, include
+from .views import hello, HelloView
+
+urlpatterns = [
+    path('hello/', hello, name='hello'),
+    path('hello-class/', HelloView.as_view(), name='hello-class'),
+]
--- myshop/views.py
+++ myshop/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
 
 # Create your views here.
+def hello(request):
+    return HttpResponse("Hello, shopper!")
+
+class HelloView(View):
+    def get(self, request):
+        return HttpResponse("Hello, Class shopper!")
